2ea27389544899481eb8817addf14dbb8a165c5b
alvaro <alraled@hotmail.es>
2016-02-23 09:57:33 +0100

    DemoMVC

---
+++ demoMVC/Controllers/AlmacenController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demoMVC.Models;
+
+namespace demoMVC.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        
+        private Tienda21Entities db=
+            new Tienda21Entities();
+        
+         // GET: Almacen
+        public ActionResult Index()
+        {
+            var data = db.Almacen;
+            return View(data);
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var data = db.Almacen.Find(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Modificar(Almacen model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State=EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+
+         }
+
+
+        public ActionResult Borrar(int id)
+        {
+            var data = db.Almacen.Find(id);
+            if (data.ProductoAlmacen.Any())
+                db.ProductoAlmacen.RemoveRange(data.ProductoAlmacen);
+            db.Almacen.Remove(data);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ demoMVC/Controllers/ProductoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demoMVC.Models;
+
+namespace demoMVC.Controllers
+{
+    public class ProductoController : Controller
+    {
+
+
+        private Tienda21Entities db = new Tienda21Entities();
+
+        public ActionResult Index()
+        {
+            var data = db.Producto;
+            return View(data);
+        }
+
+
+        public ActionResult Detalle(int id)
+        {
+            var data = db.Producto.Find(id);
+            return View(data);
+        }
+        
+        // GET: Producto
+        public ActionResult Alta()
+        {
+            return View(new Producto()); // AQUI SE PASA UN OBJETO DE TIPO PRODUCTO HACIA EL HTML //
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Producto model)         // AQUÍ NO HAY SOBRECARGA DE MÉTODOS PORQUE EL PRIMER ACTIONRESULT ESTÁ EN GET Y ÉSTE EN POST //
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(model);
+                db.SaveChanges();
+                return View(new Producto());
+            }
+
+            return View(model);
+        }
+
+    }
+}
